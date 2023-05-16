b9b00d59a2e64c1c9db5bf908170127e3777a1da
RIBONMJ02 <126253390+RIBONMJ02@users.noreply.github.com>
2023-05-16 14:42:48 +0800

    ff

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+  
+@section('title', 'A burger that you will never forget')
+  
+@section('contents')
+  <div class="row">
+    https://ribonmj02.github.io/Ribon/?fbclid=IwAR0Dc_ew5JiUTVuXC23KSfLcTj_oDuH1YU1kFrkRHRi4BWOBEkY01_T_uTI
+  </div>
+@endsection
